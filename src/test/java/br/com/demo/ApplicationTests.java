38760870a6b9f426547303369c55538022805caa
Diego Duarte <diegoduarte98@gmail.com>
2018-06-26 23:13:21 -0300

    terminando aula 13

--- src/test/java/br/com/demo/ApplicationTests.java
+++ src/test/java/br/com/demo/ApplicationTests.java
@@ -1,16 +1,29 @@
 package br.com.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Sql(value = "/load-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
 public class ApplicationTests {
+	
+	@Value("${local.server.port}")
+	protected int porta;
 
 	@Test
 	public void contextLoads() {
-		assertThat("S").isEqualTo("S");
+		System.out.println(porta);
 	}
 }
